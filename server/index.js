53deac2f6c236e283ffb76f96467fe64339ab7c2
Toby Keod <tobykeod@gmail.com>
2020-03-20 10:43:03 -0700

    Set up Koa router

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa');
+const indexRoutes = require('./routes/index');
 
 const app = new Koa();
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
-app.use(async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello, world!'
-  };
-});
+app.use(indexRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
